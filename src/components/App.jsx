a0c0966a4707fc11b95eada4afdd6f361e658cec
Post <post@Posts-MacBook-Pro-3.local>
2023-03-20 12:24:59 -0300

    ajuste para sass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import '../styles/global.scss';
 
 const App = () => {
   return (
     <div>Hola Mundo</div>
-  )
+  );
 }
 
 export default App;
